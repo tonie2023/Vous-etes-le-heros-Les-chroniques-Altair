a00a1be3c1ae77b88509bf6d8f529a626690a8af
tonie2023 <tony_19m@hotmail.ca>
2021-09-29 15:32:32 -0400

    Sauvegarde Javascript 29 septembre 2021

--- javascript.js
+++ javascript.js
@@ -1,8 +1,101 @@
-let premierChapitre = {
+let introduction = {
+  subtitle: "Introduction du héro",
+  text: `Bonjour aventuriers, aventurières! 
+         Mon nom est Malgus noid et j'ai 27 ans.
+         Je viens de la dimension Altaria où dans mon monde où la magie peut être utiliser par tous ces habitants.
+         Je suis originaire du pays Alcoria et je suis commandant de l'armée du pays.
+         Mon histoire débute lors d'une patrouille en forêt avec mes hommes où un phénomène magique se produisit me séparant de mes hommes et me faisant perdre connaissance.
+         À mon réveil, j'étais dans une cellule où de l'autre côté des barreaux
+        une femme en armure posa la question suivante : Qui êtes-vous ?`,
+  img:,
+}
+
+let reveilHero = {
   subtitle: "Réveil du héro",
+  text: `Le commandant Malgus d'Astoria prit dans un phénomène magique inconnu perdit connaissance pour se reprendre conscience dans une cellule.
+  Confus de sa situation problématique. il reprit pied dans la réalité lorsqu'une voix  de l'autre côté des barreaux lui demanda: "Qui êtes-vous et d'où venez-vous?
+  Malgus se retourna pour découvrir une femme en armure s'adressant à lui.`,
+  img:,
+  option:,
+};
+let souschap1 = {
+  subtitle:,
   text:,
   img:,
-};
+  option:,
+}
+let souschap2 = {
+  subtitle:,
+  text:,
+  img:,
+  option:,
+}
+let souschap3 = {
+  subtitle:,
+  text:,
+  img:,
+  option:,
+}
+let chap2= {
+  subtitle:,
+  text:,
+  img:,
+  option:,
+}
+let souschap2_1 = {
+  subtitle:,
+  text:,
+  img:,
+  option:,
+}
+let souschap2_2 = {
+  subtitle:,
+  text:,
+  img:,
+  option:,
+}
+let chap3= {
+  subtitle:,
+  text:,
+  img:,
+  option:,
+}
+let souschap3_1 = {
+  subtitle:,
+  text:,
+  img:,
+  option:,
+}
+let souschap3_2 = {
+  subtitle:,
+  text:,
+  img:,
+  option:,
+}
+let chap4 = {
+  subtitle:,
+  text:,
+  img:,
+  option:,
+}
+let chap5 = {
+  subtitle:,
+  text:,
+  img:,
+  option:,
+}
+let chap6 = {
+  subtitle:,
+  text:,
+  img:,
+  option:,
+}
+let epilogue = {
+  subtitle:,
+  text:,
+  img:,
+  option:,
+}
 let chaptersObj = {
-    slug:,
+    :,
 }
