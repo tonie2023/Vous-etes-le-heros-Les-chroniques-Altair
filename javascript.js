49b8934e0a66c7b8e13a67964e9bfe82c849cf4d
tonie2023 <tony_19m@hotmail.ca>
2021-09-30 10:02:51 -0400

    Sauvegarde première partie des chapitres et sous-chapitres

--- javascript.js
+++ javascript.js
@@ -2,9 +2,9 @@ let introduction = {
   subtitle: "Introduction du héro",
   text: `Bonjour aventuriers, aventurières! 
          Mon nom est Malgus noid et j'ai 27 ans.
-         Je viens de la dimension Altaria où dans mon monde où la magie peut être utiliser par tous ces habitants.
+         Je viens de la dimension Altaria où dans mon monde la magie peut être utiliser par tous ces habitants.
          Je suis originaire du pays Alcoria et je suis commandant de l'armée du pays.
-         Mon histoire débute lors d'une patrouille en forêt avec mes hommes où un phénomène magique se produisit me séparant de mes hommes et me faisant perdre connaissance.
+         Mon histoire débuta lors d'une patrouille en forêt avec mes hommes où un phénomène magique se produisit me séparant de mes hommes et me faisant perdre connaissance.
          À mon réveil, j'étais dans une cellule où de l'autre côté des barreaux
         une femme en armure posa la question suivante : Qui êtes-vous ?`,
   img:,
@@ -12,31 +12,62 @@ let introduction = {
 
 let reveilHero = {
   subtitle: "Réveil du héro",
-  text: `Le commandant Malgus d'Astoria prit dans un phénomène magique inconnu perdit connaissance pour se reprendre conscience dans une cellule.
-  Confus de sa situation problématique. il reprit pied dans la réalité lorsqu'une voix  de l'autre côté des barreaux lui demanda: "Qui êtes-vous et d'où venez-vous?
-  Malgus se retourna pour découvrir une femme en armure s'adressant à lui.`,
+  text: `Le commandant Malgus d'Astoria prit dans un phénomène magique inconnu perdit connaissance pour reprendre conscience dans une cellule.
+  Confus de sa situation problématique. il reprit pied dans la réalité lorsqu'une voix de l'autre côté des barreaux 
+  lui demanda: "Qui êtes-vous et d'où venez-vous? Malgus se retourna pour découvrir une femme en armure s'adressant à lui. 
+  La femme répéta la question : "Qui êtes-vous et d'où venez-vous?" `,
   img:,
-  option:,
+  option: ["Le héro lui répond", "Le héro mentit"],
 };
-let souschap1 = {
-  subtitle:,
+let interrogatoireMalgus = {
+  subtitle: "Amavia",
+  text: `Malgus choisit l'honnêteté. "Je m'appelle Malgus, je suis le commandant de l'armée d'Alcoria.
+   J'étais en patrouille avec mes hommes lorsqu'un phénomène magique inconnu est survenu ce qui me fit perdre connaissance.
+   À mon réveil, j'étais dans cette cellule. Pouvez-vous me dire où je suis et pourquoi, s'il-vous-plait? Cela m'aiderait à comprendre ma situation actuelle."
+   La femme en armure répondit : "Je m'appelle Amavia, je suis la commandante de l'armée du royaume de Magnolia. Quant au pourquoi de votre situation,
+   il s'agit d'une mesure préventive car voyez-vous nous sommes en guerre et mes hommes vous on trouvez inconscient sur notre territoire pensant que vous êtes un espion. L'êtes-vous ?"
+   Malgus lui répondit : "Non commandante, je ne suis pas un espion." dit-il d'un air résolu. La commandante lui-répondit: "C'est ce que je vais déterminé à l'instant. "`,
+  img:,
+  option: ["Amavia décide de croire l'histoire de Malgus", "Amavia ne croit pas l'histoire de Malgus"],
+}
+let inquisition = {
+  subtitle:"Malhonnêteté inopportune",
+  text:`Malgus choisit le mensonge. "Je m'appelle Malgis, je suis marchand agricole.
+  J'étais en route avec mes produits agricole vers le marché de la capitale lorsque des brigands m'ont attaqué, pillé et assommé par la suite.
+  À mon réveil, j'étais dans cette cellule. Pouvez-vous me dire où je suis et pourquoi, s'il-vous-plait? 
+  La femme en armure répondit : "Je m'appelle Amavia, je suis la commandante de l'armée du royaume de Magnolia. Quant au pourquoi de votre situation,
+  il s'agit d'une mesure préventive car voyez-vous nous sommes en guerre et mes hommes vous on trouvez inconscient sur le territoire de la captitale pensant que vous êtes un espion. L'êtes-vous ?"
+  Malgus lui répondit : "Non commandante, je ne suis pas un espion." dit-il d'un air défiant. La commandante lui-répondit: "C'est ce que je vais déterminé à l'instant." 
+  Par contre, intérieurement, elle se dit : "Son histoire ne colle pas, quand on l'a retrouvé inconscient, il portait des vêtements à mi-chemin entre des vêtements normaux et d'une armure.
+  Que dois faire, le croire ou non ?"`,
+  img:,
+  option: ["Amavia décide de croire l'histoire de Malgis(Malgus)", "Amavia ne croit pas l'histoire de Malgis(Malgus)"],
+}
+let destinPositif = {
+  subtitle: "Libération providentielle",
+  text: `Notre héro en cellule attendait la réponse de la commandante Amavia. 
+  Après 5 minutes de réflexion, elle donna sa décision : "Malgus, après ce que vous venez de me révéler sur vous, j'ai pesé le pour et le contre et j'ai choisis de vous croire.".
+  Elle ouvrit la porte de la cellule et dit : "Maintenant, laisser-moi vous éclairer sur la situation actuelle du royaume. 
+  Ensuite, j'aurais une proposition à vous faire.".`,
+  img:,
+  option: ["Amavia offre à Malgus d'intégrer son armée", "Rester en cellule une journée de plus"],
+}
+let destinNégatif = {
+  subtitle: `Partie terminé`,
+  text: `Notre héro en cellule attendait la réponse de la commandante Amavia avec nervosité. 
+  Après 5 minutes de réflexion, elle donna sa décision : "Malgus, après ce que vous venez de me révéler sur vous, j'ai pesé le pour et le contre et je ne vous crois pas.".
+  Malgis(Malgus) : "Mais, pourquoi!?" dit-il d'un ton plaintif. Amavia répondit : "Il y a des éléments qui ne marchent pas dans votre histoire, 
+  notamment vos vêtements inhabituels ne correspondant pas à un marchand agricole, je suis désolé mais on va devoir vous garder plus longtemps dans votre cellule.". Malgis(Malgus) cria : "Non!!!".`, 
+  img:,
+  option: ["La partie est terminé, veuillez recommencer une nouvelle partie!"],
+}
+let insertionHéroïque= {
+  subtitle: `Magnolia`,
   text:,
   img:,
   option:,
 }
-let souschap2 = {
-  subtitle:,
-  text:,
-  img:,
-  option:,
-}
-let souschap3 = {
-  subtitle:,
-  text:,
-  img:,
-  option:,
-}
-let chap2= {
+let assignationFaction= {
   subtitle:,
   text:,
   img:,
@@ -54,7 +85,7 @@ let souschap2_2 = {
   img:,
   option:,
 }
-let chap3= {
+let premiersCombats= {
   subtitle:,
   text:,
   img:,
@@ -72,19 +103,19 @@ let souschap3_2 = {
   img:,
   option:,
 }
-let chap4 = {
+let alliance= {
   subtitle:,
   text:,
   img:,
   option:,
 }
-let chap5 = {
+let consécration = {
   subtitle:,
   text:,
   img:,
   option:,
 }
-let chap6 = {
+let paix= {
   subtitle:,
   text:,
   img:,
