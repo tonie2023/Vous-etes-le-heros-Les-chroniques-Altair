f1d7718f6ec906444726d4ab04c7d23d8c32aff8
tonie2023 <tony_19m@hotmail.ca>
2021-11-18 10:44:22 -0500

    Sauvegarde audio partie 4.1

--- javascript.js
+++ javascript.js
@@ -340,7 +340,7 @@ function decision4() {
   }
 }
 
-let audio = new Audio("assets/boingCartoon7.mp3");
+let audio = new Audio("assets/batman.mp3");
 
 function goToChapter(chapterName) {
   let chapitre = chaptersObj[chapterName];
